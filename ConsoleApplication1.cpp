ea700c50d4a4908fb75fc6a76937f69bc43f9d27
GOODDDLIFE <101847716+GOODDDLIFE@users.noreply.github.com>
2022-03-31 01:09:16 +0900

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("input number a,b\n: ");
+    scanf_s("%d %d",&x,&y);
+
+    printf("%d + %d = %d\n", x, y, x + y);
+    printf("%d - %d = %d\n", x, y, x - y);
+    printf("%d x %d = %d\n", x, y, x * y);
+
+    return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main()
+{
+    printf("CHAR MIN = %lf\n ", SCHAR_MIN);
+    printf("CHAR MAX = %lf\n ", SCHAR_MAX);
+    printf("INT MIN =  %lf\n ", SHRT_MIN);
+    printf("INT MAX =  %lf\n ", SHRT_MAX);
+    printf("DOUBLE MIN = %lf\n ", DBL_MIN);
+    printf("DOUBLE MAX = %lf\n ", DBL_MAX);
+    printf("FLOAT MIN = %lf\n", FLT_MIN);
+    printf("FLOAT MAX = %lf\n ", FLT_MAX); // 각각의 자료형에 대한 저장되어있는 최대/최솟값을 printf로 출력
+    return 0;
+}
